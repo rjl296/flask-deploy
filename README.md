20a0c40165be1205c885306f87bb4fb08be8ec8e
rjl296 <lo.ryanlo@gmail.com>
2021-12-16 20:18:23 +0800

    3d commit

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # flask-deploy
-# flask-deploy
-# flask-deploy
-# flask-deploy
+
--- tasks/celery_worker.py
+++ tasks/celery_worker.py
@@ -1,6 +1,6 @@
 #This module is required to start and initialize a Celery worker, which will run in a separate Docker container. It initializes the Flask application context to have access to the same environment as the application. If that’s not required, these lines can be safely removed.
 
-from app import create_app
+from application import create_app
 
 app = create_app()
 app.app_context().push()
